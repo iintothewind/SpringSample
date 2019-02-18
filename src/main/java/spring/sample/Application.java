0ecdf61afbd1b7ccee5930fb4bdb5f8a2c946f7c
ivar <yinghchen@paypal.com>
2019-02-18 23:48:19 +0800

    added spring web

--- src/main/java/spring/sample/Application.java
+++ src/main/java/spring/sample/Application.java
@@ -1,9 +1,13 @@
 package spring.sample;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 
+@EnableAutoConfiguration
+@ComponentScan
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
--- src/main/java/spring/sample/service/AsyncService.java
+++ src/main/java/spring/sample/service/AsyncService.java
@@ -1,17 +1,25 @@
 package spring.sample.service;
 
+import javax.servlet.http.HttpServletRequest;
 import org.apache.logging.log4j.util.Strings;
 import org.springframework.retry.annotation.Retryable;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.stereotype.Component;
 import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 import lombok.extern.slf4j.Slf4j;
 
 
 @Slf4j
 @Component
 public class AsyncService {
+    public static HttpServletRequest loadReq() {
+        final ServletRequestAttributes servletRequestAttributes = (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
+        return servletRequestAttributes.getRequest();
+    }
+
     @Async
     @Retryable(include = { RuntimeException.class })
     public ListenableFuture<Integer> execute(Integer i) {
@@ -29,5 +37,9 @@ public class AsyncService {
             throw new IllegalArgumentException("empty string");
         }
         return msg;
+    }
+
+    public void specReq() {
+        log.info(loadReq().getRequestURI());
     }
 }
